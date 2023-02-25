84d532889293adfb6b80fda79e0a93a2c7f2bcd1
unknown <benbushman98@gmail.com>
2023-02-25 13:36:09 -0700

    changes file structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './docs/index.css';
+import App from './docs/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
