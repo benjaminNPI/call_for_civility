03c5c0e79b902167df575e570ad5e14599b13e26
unknown <benbushman98@gmail.com>
2023-03-06 19:38:36 -0700

    added backend express

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   }
   // fetching the GET route from the Express server which matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/');
+    const response = await fetch('/express_backend');
     const body = await response.json();
 
     if (response.status !== 200) {
