634ffa589d6e1b75b42f3e21af0ee97c35ef9d50
Ben Bushman <bbushman@AHSMAIL.COM>
2023-03-07 10:20:44 -0700

    added routes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 
+
+
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3002; //Line 3
+
+
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
@@ -14,6 +18,7 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
