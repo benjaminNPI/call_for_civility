532a47219edb7c168fdd67a341dc1c55e79672fb
unknown <benbushman98@gmail.com>
2023-03-06 19:52:37 -0700

    added code

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || 5000; //Line 3
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello from server!" });
+// });
 
 app.get('https://callforcivility.com/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
