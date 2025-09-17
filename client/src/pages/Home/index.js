4622a7051a9eb9988edb2caeb2ac11e8bd2e5490
Benjamin Bushman <benbushman98@gmail.com>
2025-09-16 22:23:53 -0600

    feat: added link to news article

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,41 +1,66 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-    document.title = "Home | Call for Civility"
-    return (
-        <>
-            <div className={`justify-center bg-no-repeat bg-cover bg-center rounded-lg capital`}>
-                <main className="">
-                    <div className="relative px-2 lg:px-2">
-                        <div className="mx-auto max-w-xl py-24 sm:py-32 lg:py-40">
-
-                            <div className="text-center">
-                                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                    A Clarion Call for Civility
-                                </h1>
-                                <p className="mt-6 text-5xl leading-8 text-black-900">
-                                <strong>In a country troubled with wars, hatred, and evil, we call on <strong>YOU</strong> to join the movement for civility and honor in our nation.</strong>
-                                </p>
-                                <div className="mt-10 flex items-center justify-center gap-x-6">
-                                    <Link
-                                        to="/pledge"
-                                        className="rounded-md bg-blue-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    >
-                                        Sign the Pledge
-                                    </Link>
-                                    <Link to="/letter-clarion-call-for-civility" className="font-semibold leading-6 text-gray-900 hover:text-gray-600">
-                                        Read the Call <span aria-hidden="true">→</span>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
+  document.title = "Home | Call for Civility";
+  return (
+    <>
+      <div
+        className={`justify-center bg-no-repeat bg-cover bg-center rounded-lg capital`}
+      >
+        <main className="">
+          <div className="relative px-2 lg:px-2">
+            <div className="mx-auto max-w-xl py-24 sm:py-32 lg:py-40">
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  A Clarion Call for Civility
+                </h1>
+                <p className="mt-6 text-5xl leading-8 text-black-900">
+                  <strong>
+                    In a country troubled with wars, hatred, and evil, we call
+                    on <strong>YOU</strong> to join the movement for civility
+                    and honor in our nation.
+                  </strong>
+                </p>
+                <p className="mt-6 text-lg leading-8">
+                  <strong>
+                    Read the{" "}
+                    <strong>
+                      <span>
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          href="https://yournews.com/2025/09/12/3868302/campaignforcivility-com-launches-nationwide-movement/"
+                          className="underline"
+                        >
+                          featured article
+                        </a>
+                      </span>
+                    </strong>{" "}
+                    to learn more about the movement.
+                  </strong>
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <Link
+                    to="/pledge"
+                    className="rounded-md bg-blue-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign the Pledge
+                  </Link>
+                  <Link
+                    to="/letter-clarion-call-for-civility"
+                    className="font-semibold leading-6 text-gray-900 hover:text-gray-600"
+                  >
+                    Read the Call <span aria-hidden="true">→</span>
+                  </Link>
+                </div>
+              </div>
             </div>
-        </>
-    );
-}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 
-
-export default Home
+export default Home;
